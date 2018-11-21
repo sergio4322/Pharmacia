52584c33ea93caa9549f209e467830426ac6ef43
sergio4322 <sergio_ush_15@hotmail.com>
2018-11-20 21:44:24 -0300

    Instale Controllador PharmaciaBundle:Farmacia

---
+++ src/PharmaciaBundle/Controller/PacienteController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PharmaciaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PacienteController extends Controller
+{
+}
